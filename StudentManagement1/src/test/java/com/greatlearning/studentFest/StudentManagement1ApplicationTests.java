eac53babae48afe81d8d55638f4cf4378aae2b57
frenygada23 <94836026+frenygada23@users.noreply.github.com>
2022-04-10 21:36:02 +0530

    file upload

---
+++ StudentManagement1/src/test/java/com/greatlearning/studentFest/StudentManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.studentFest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
