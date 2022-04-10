bfd8821d3b4b0a704a17b2d9f5999e8a99e598c1
frenygada23 <94836026+frenygada23@users.noreply.github.com>
2022-04-10 21:38:34 +0530

    Add files via upload

---
+++ sql1.sql
@@ -0,0 +1,8 @@
+create database studentdb2;
+use studentdb2;
+
+insert into users values(1,'$2a$12$A.atCQs3N7jbExdmDL3qW.7WvWL6Petx.1IoL2ZuXghry6KVpQIK','user1');
+insert into users values(2,'$2a$12$A.atCQs3N7jbExdmDL3qW.7WvWL6Petx.1IoL2ZuXghry6KVpQIK','user2');
+
+insert into users_roles values (1,1);
+insert into users_roles values (2,2);
